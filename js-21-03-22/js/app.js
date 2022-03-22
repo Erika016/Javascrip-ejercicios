4c3bdcaba03526a30d24981a76b677a90ed0553c
erika <ericadominguezbarrientos@gmail.com>
2022-03-22 20:18:49 +0100

    ejercicio 10 revisado

--- js-21-03-22/js/app.js
+++ js-21-03-22/js/app.js
@@ -139,10 +139,28 @@
 //     alert("Cool down");
 //     break;
 //   default:
+//     alert("Write a real data");
 //     break;
 // }
 
-// // EJERCICIO 4
+// let temperature = prompt("¿What temperature is it?");
+// console.log(temperature);
+// switch (true) {
+//   case temperature < "15":
+//     alert("Warm up");
+//     break;
+//   case temperature >= "15" && temperature <= "25":
+//     alert("Enjoy the weather!");
+//     break;
+//   case temperature > "25":
+//     alert("Cool down");
+//     break;
+//   default:
+//     alert ("Write a real data!");
+//     break;
+// }
+
+// EJERCICIO 4
 
 // let isGreen = prompt ("what color is the traffic light?");
 // console.log(isGreen);
@@ -150,25 +168,257 @@
 //   case "green":
 //     console.log("green", "Cross the road");
 //     break;
-//     default:"error";
+//     default:
 //     break;
 // }
 // console.log("Please await");
 
 // EJERCICIO 5
 
-// const fahrenheit = prompt("Convert to Fahrenheit");
-let celsius =parseFloat(prompt("Concert to fahrenheit"));
-const fahrenheit = (9/5*celsius) + 32;
-console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
-switch (celsius,fahrenheit){
-  case "Absolute Zero (-274,15) $celsius = $fahrenheit":
-    break;
-    case "FreezingPoint (0) $celsius = $fahrenheit":
-      break
-      case "Body Temperature (37) $celsius = $fahrenheit":
-        break
-        case "Boiling point(100) $celsius = $fahrenheit":
-        break;
+// const CELSIUS = prompt("Write temperature in grades centigrades");
+// // let celsius = parseFloat(prompt("Convert to fahrenheit"));
+// const FAHRENHEIT = (CELSIUS * 9) / 5 + 32;
+// console.log(
+//   `${CELSIUS} degree celsius is equal to ${FAHRENHEIT} degree fahrenheit.`
+// );
+// switch ((celsius, fahrenheit)) {
+//   case "Absolute Zero (-274,15) $celsius = $fahrenheit":
+//     break;
+//   case "FreezingPoint (0) $celsius = $fahrenheit":
+//     break;
+//   case "Body Temperature (37) $celsius = $fahrenheit":
+//     break;
+//   case "Boiling point(100) $celsius = $fahrenheit":
+//     break;
+// }
 
+// SOLUCION EJERCICIO 5
+// let temperature = "Absolute Zero";
+// switch (temperature) {
+//   case "Absolute Zero":
+//     (-273.15*9) /5+32;
+// break;
+// }
+
+// EJERCICIO 6
+
+// EJERCICIO 7 IGUAL QUE EL 5
+
+// Evaluating calificaations
+// let calification = prompt("Please, enter your calification:");
+// let insuficiente = "<5";
+// let suficiente = "=>5";
+// let bien = 6;
+// let notable = 7 ==8;
+// let sobresaliente = 9==10;
+// switch (parseInt(calification)) {
+//   case 0:
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//     console.log("insuficiente");
+//     break;
+//     case 5:
+//       console.log ("suficiente");
+//       break;
+//       case 6:
+//         console.log("bien");
+//       case 7:
+//         case 8:
+//           console.log("notable");
+//           break;
+//           case 9:
+//             case 10:
+//               console.log("sobresaliente");
+//               break;
+
+// }
+
+// 
+
+// EJERCICIO 9
+// let data = prompt("Please enter DNI or NIE:");
+// if (data.length == 9 ) {
+//   if (
+//     // data.charCodeAt(0) == 88 || data.charCodeAt(0) == 84) {
+//     // if (data.charCodeAt(data.length-1) >=65 &&
+//     // data.charCodeAt(data.length-1) <=90
+//     65 <= 
+//     datadata.charCodeAt(data.length -1) <= 90
+//      ) {
+//        if (
+//          data.charCodeAt(1) >= 48 && 
+//          data.charCodeAt(1) <= 57 &&
+//          ) }
+//          console.log("valid NIE");
+//   }
+
+// } else {
+//   console-log("Please enter valid data");
+// }
+// //  Resultado Fran
+// let userNieDni = prompt("Please, write your DNI or NIE");
+// if (userNieDni.length == 9) {
+//  let firstCharacter = userNieDni.charCodeAt(0);
+//  let lastCharacter = userNieDni.charCodeAt(userNieDni.length - 1);
+//  //if NIE
+//  if (
+//  (firstCharacter == 84 || firstCharacter == 88) &&
+//  !isNaN(userNieDni.substring(1, userNieDni.length - 1)) &&
+//  lastCharacter >= 65 &&
+//  lastCharacter <= 90
+//  ) {
+//  console.log("Your NIE is valid");
+//  }
+//  // if DNI
+//  else if (
+//  !isNaN(userNieDni.substring(0, userNieDni.length - 1)) &&
+//  lastCharacter >= 65 &&
+//  lastCharacter <= 90
+//  ) {
+//  console.log("Your DNI is valid");
+//  } else {
+//  console.log("Your DNI or NIE is not valid");
+//  }
+// } else {
+//  console.log("Your DNI or NIE is not valid");
+// }
+
+// EJERCICIO 9
+ 
+// SOLUCION KAY
+//Comprobador de DNI
+// let identificacion = prompt("¿Se va a registrar con el DNI o con el NIE?");
+// switch (identificacion) {
+//  case "DNI":
+//  var DNI = prompt("Introduzca su DNI");
+//  if (
+//  DNI.charCodeAt(0) >= 48 &&
+//  DNI.charCodeAt(0) <= 57 &&
+//  DNI.charCodeAt(1) >= 48 &&
+//  DNI.charCodeAt(1) <= 57 &&
+//  DNI.charCodeAt(2) >= 48 &&
+//  DNI.charCodeAt(2) <= 57 &&
+//  DNI.charCodeAt(3) >= 48 &&
+//  DNI.charCodeAt(3) <= 57 &&
+//  DNI.charCodeAt(4) >= 48 &&
+//  DNI.charCodeAt(4) <= 57 &&
+//  DNI.charCodeAt(5) >= 48 &&
+//  DNI.charCodeAt(5) <= 57 &&
+//  DNI.charCodeAt(6) >= 48 &&
+//  DNI.charCodeAt(6) <= 57 &&
+//  DNI.charCodeAt(7) >= 48 &&
+//  DNI.charCodeAt(7) <= 57 &&
+//  DNI.charCodeAt(8) >= 65 &&
+//  DNI.charCodeAt(8) <= 90
+//  ) {
+//  alert("You logged correctly");
+//  } else {
+//  alert("Your DNI number is incorrect");
+//  }
+//  break;
+//  case "NIE":
+//  var NIE = prompt("Introduzca su NIE");
+//  if (
+//  (NIE.charCodeAt(0) == 84,
+//  NIE.charCodeAt(0) == 88 &&
+//  NIE.charCodeAt(1) >= 48 &&
+//  NIE.charCodeAt(1) <= 57 &&
+//  NIE.charCodeAt(2) >= 48 &&
+//  NIE.charCodeAt(2) <= 57 &&
+//  NIE.charCodeAt(3) >= 48 &&
+//  NIE.charCodeAt(3) <= 57 &&
+//  NIE.charCodeAt(4) >= 48 &&
+//  NIE.charCodeAt(4) <= 57 &&
+//  NIE.charCodeAt(5) >= 48 &&
+//  NIE.charCodeAt(5) <= 57 &&
+//  NIE.charCodeAt(6) >= 48 &&
+//  NIE.charCodeAt(6) <= 57 &&
+//  NIE.charCodeAt(7) >= 48 &&
+//  NIE.charCodeAt(7) <= 57 &&
+//  NIE.charCodeAt(8) >= 65 &&
+//  NIE.charCodeAt(8) <= 90)
+//  ) {
+//  alert("You logged correctly");
+//  }else {
+//  alert("Your NIE number is incorrect");
+//  }
+//  break;
+//  default:
+//  alert("Incorrect option");
+// }//Comprobador de DNI
+// let identificacion = prompt("¿Se va a registrar con el DNI o con el NIE?");
+// switch (identificacion) {
+//  case "DNI":
+//  var DNI = prompt("Introduzca su DNI");
+//  if (
+//  DNI.charCodeAt(0) >= 48 &&
+//  DNI.charCodeAt(0) <= 57 &&
+//  DNI.charCodeAt(1) >= 48 &&
+//  DNI.charCodeAt(1) <= 57 &&
+//  DNI.charCodeAt(2) >= 48 &&
+//  DNI.charCodeAt(2) <= 57 &&
+//  DNI.charCodeAt(3) >= 48 &&
+//  DNI.charCodeAt(3) <= 57 &&
+//  DNI.charCodeAt(4) >= 48 &&
+//  DNI.charCodeAt(4) <= 57 &&
+//  DNI.charCodeAt(5) >= 48 &&
+//  DNI.charCodeAt(5) <= 57 &&
+//  DNI.charCodeAt(6) >= 48 &&
+//  DNI.charCodeAt(6) <= 57 &&
+//  DNI.charCodeAt(7) >= 48 &&
+//  DNI.charCodeAt(7) <= 57 &&
+//  DNI.charCodeAt(8) >= 65 &&
+//  DNI.charCodeAt(8) <= 90
+//  ) {
+//  alert("You logged correctly");
+//  } else {
+//  alert("Your DNI number is incorrect");
+//  }
+//  break;
+//  case "NIE":
+//  var NIE = prompt("Introduzca su NIE");
+//  if (
+//   //  LETRAS DNI NUMERICO charCodeAt cambia numero por letras
+//  (NIE.charCodeAt(0) == 84, NIE.charCodeAt(0) == 88) &&
+//  NIE.charCodeAt(1) >= 48 &&
+//  NIE.charCodeAt(1) <= 57 &&
+//  NIE.charCodeAt(2) >= 48 &&
+//  NIE.charCodeAt(2) <= 57 &&
+//  NIE.charCodeAt(3) >= 48 &&
+//  NIE.charCodeAt(3) <= 57 &&
+//  NIE.charCodeAt(4) >= 48 &&
+//  NIE.charCodeAt(4) <= 57 &&
+//  NIE.charCodeAt(5) >= 48 &&
+//  NIE.charCodeAt(5) <= 57 &&
+//  NIE.charCodeAt(6) >= 48 &&
+//  NIE.charCodeAt(6) <= 57 &&
+//  NIE.charCodeAt(7) >= 48 &&
+//  NIE.charCodeAt(7) <= 57 &&
+//  NIE.charCodeAt(8) >= 65 &&
+//  NIE.charCodeAt(8) <= 90)
+//  {
+//  alert("You logged correctly");
+//  }else {
+//  alert("Your NIE number is incorrect");
+//  }
+//  break;
+//  default:
+//  alert("Incorrect option");
+// }
+
+// EJERCICIO 10
+
+let max =50;
+let min =1;
+let randomNumber1 = Math.floor (Math.random()*max)+min;
+let randomNumber2 = Math.floor (Math.random()*max)+min;
+if (randomNumber1 > randomNumber2){
+  console.log("Player 1 cins!");
+} else if (randomNumber1 < randomNumber2){
+  console.log("player 2 wins!");
+} else {
+  console.log("Draw");
 }
+console-console.log((randomNumber1));
