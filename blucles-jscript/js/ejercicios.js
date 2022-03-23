ed600a9fb3aaecd6f1495480aba460396d570142
erika <ericadominguezbarrientos@gmail.com>
2022-03-24 00:21:51 +0100

    ejercicio7mas codigos copiados

--- blucles-jscript/js/ejercicios.js
+++ blucles-jscript/js/ejercicios.js
@@ -1,4 +1,22 @@
-// EJERCICIO1
-let text="palindrome";
-console.log(typeof text);
+// // EJERCICIO1
+// let text="palindrome";
+// // // console.log(typeof text);
+// // text.charCodeAt();
 
+// for (text.length; text.charAt = ; ){
+// console.log(text.charAt(2) + text.length);
+// }
+
+// let max =50;
+let max = 13;
+let min = 1;
+let randomNumber1 = Math.floor (Math.random()*max)+min;
+let randomNumber2 = Math.floor (Math.random()*max)+min;
+if (randomNumber1 > randomNumber2){
+  console.log("Player 1 cins!");
+} else if (randomNumber1 < randomNumber2){
+  console.log("player 2 wins!");
+} else {
+  console.log("Draw");
+}
+console-console.log((randomNumber1));
