2aed8ea38354b40a3428b77af04f0ac702066aaa
erika <ericadominguezbarrientos@gmail.com>
2022-03-31 20:36:13 +0200

    exempl. dom

--- dom/js/app.js
+++ dom/js/app.js
@@ -1,1 +1,10 @@
-console.log("working!");
+let nav = document.querySelector('nav');
+console.log(nav);
+// let active = document.querySelcetor('.active');
+// console.log(active);
+let paragraph = document.querySelector('#first');
+console.log(paragraph);
+
+let headersAndParagraphs = document.querySelectorAll(`h1,p`);
+console.log(headersAndParagraphs[0].innerText);
+console.log(active.innerText);
