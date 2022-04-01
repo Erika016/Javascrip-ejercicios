cdda82511583828ef260f89bd38e8316ba7c9eac
erika <ericadominguezbarrientos@gmail.com>
2022-04-01 20:40:56 +0200

    empezamos a usar javaScript junto con HTML y CSS

--- dom/js/app.js
+++ dom/js/app.js
@@ -1,10 +1,21 @@
-let nav = document.querySelector('nav');
-console.log(nav);
-// let active = document.querySelcetor('.active');
-// console.log(active);
-let paragraph = document.querySelector('#first');
-console.log(paragraph);
+// let nav = document.querySelector('nav');
+// // console.log(nav);
+// // let active = document.querySelcetor('.active');
+// // console.log(active);
+// let paragraph = document.querySelector('#first');
+// // console.log(paragraph);
+// let headersAndParagraphs = document.querySelectorAll(`h1,p`);
+// // console.log(headersAndParagraphs[1].innerText);
+// // console.log(headersAndParagraphs);
+// // accesing nodes without text
+// // console.log(nav.children[0].children[0].children[0]);
+// // console.log(headersAndParagraphs[0].nextElementSibling.nextElementSibling);
+// console.log(headersAndParagraphs[0].firstChild);
+// console.log(headersAndParagraphs[0].firstChild.nodeValue);
+// console.log(nav.nodeName);
+let box1 = document.querySelector("section").firstElementChild;
+let box2 =box1.nextElementSibling;
+let box3 = box2.nextElementSibling;
 
-let headersAndParagraphs = document.querySelectorAll(`h1,p`);
-console.log(headersAndParagraphs[0].innerText);
-console.log(active.innerText);
+box1.classList.add("dark");
+box1.classList.remove("dark");
