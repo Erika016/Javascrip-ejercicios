706737368e8128911f77123c39c7454f30657cbf
erika <ericadominguezbarrientos@gmail.com>
2022-04-02 11:42:01 +0200

    algoritms3

---
+++ algorithms3.js/js/app.js
@@ -0,0 +1,5 @@
+// Exercise 1
+let number1 = prompt("indique un numero");
+let number2 = prompt("indique un numero mayor al que ha introducido")
+let toGues =Array.of[Math.floor(Math.random() *11)];
+console.log(toGues);
