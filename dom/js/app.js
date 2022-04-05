5c28c4f769044d3f9ce9b577f93f60aa27223ecf
erika <ericadominguezbarrientos@gmail.com>
2022-04-05 20:38:04 +0200

    teoria de DOM dia 05-04-22 despues del descanso

--- dom/js/app.js
+++ dom/js/app.js
@@ -14,8 +14,48 @@
 // console.log(headersAndParagraphs[0].firstChild.nodeValue);
 // console.log(nav.nodeName);
 let box1 = document.querySelector("section").firstElementChild;
-let box2 =box1.nextElementSibling;
+let box2 = box1.nextElementSibling;
 let box3 = box2.nextElementSibling;
 
-box1.classList.add("dark");
-box1.classList.remove("dark");
+// box1.classList.add("dark");
+// box1.classList.remove("dark");
+// box2.classList.add("dissapear");
+// box2.classList.remove("dissapear");
+
+// box2.classList.toggle("light");
+// console.log(box3.classList.contains("light"));
+// console.log(box2.classList.contains("light"));
+// let number =
+// document.querySelector("section").lastElementChild.previousElementSibling.dateset.boxNumber;
+// console.log(number);
+// document.body.innerHTML = "<h1>Waw!</h1>"
+// console.log(document.querySelector("section").outerHTML);
+// document.querySelector("section").outerHTML= "<h1>Waw!</h1>"
+// document.body.insertAdjacentHTML('afterbegin', '<nav>Navigation</nav>')
+// document
+// .querySelector("nav")
+// .insertAdjacentHTML("beforebegin","<h1>Main Header</h1>");
+// document
+// .querySelector("nav")
+// .insertAdjacentHTML("beforeend", ' <a href="http://google.com">Go Google</a>')
+// document
+// .querySelector("nav")
+// .insertAdjacentHTML("afterend", '<p>Lorem ipsum dolor sit amet.</p>')
+// console.log(document.querySelector("h1").textContent);
+// document.querySelector("nav").textContent= "Stronger together";
+
+
+
+
+
+let header = document.createElement("h1")
+header.append("Hello,Word!")
+header.prepend("¡");
+document.body.append(header);
+document.querySelector("h1").firstChild.before("¡¡");
+let paragraph = document.createElement("p");
+paragraph.textContent = "Writting a paragraph";
+// document.body.append(paragraph);
+document.querySelector("h1").after(paragraph);
+document.querySelector("h1").replaceWith(paragraph);
+// paragraph.remove();PARA ELIMINAR EL PARRAFO
